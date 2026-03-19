315b7af9fa745698e978f684bc2940f2fa8abb0f
Beatrice Amolo <amollobetty@gmail.com>
2026-03-19 06:17:50 +0300

    first commit

---
+++ exercise2.1.py
@@ -0,0 +1,12 @@
+# Import the math module to access pi
+import math
+
+# Radius of the sphere in centimeters
+radius = 5  
+
+# Volume of a sphere formula: (4/3) * pi * r^3
+# Volume in cubic centimeters
+volume = (4/3) * math.pi * radius**3  
+
+# Display the result
+print(volume)
