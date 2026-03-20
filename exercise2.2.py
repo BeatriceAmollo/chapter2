d9a644691115a27dac44da752a8c32cd6574cc8a
Beatrice Amolo <amollobetty@gmail.com>
2026-03-20 16:26:33 +0300

    exercise2.2.py

---
+++ exercise2.2.py
@@ -0,0 +1,13 @@
+# Value of x
+x = 42  
+import math
+
+# Compute sine and cosine
+sin_x = math.sin(x)
+cos_x = math.cos(x)
+
+# Sum of their squares
+result = sin_x**2 + cos_x**2
+
+# Display the result
+print(result)
---
+++ exercise2.3.py
@@ -0,0 +1,16 @@
+# Method 1: Using exponentiation operator
+import math
+method1 = math.e ** 2
+
+print(method1)
+
+# Method 2: Using math.pow
+import math
+method2 = math.pow(math.e, 2)
+
+print(method2)
+
+# Method 3: Using math.exp
+import math
+method3 = math.exp(2)
+print(method3)
